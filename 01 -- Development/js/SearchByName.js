c2237181571b24f7a44eeefc5eb0ce1b940b388e
Eliphas-art <52660922+Eliphas-art@users.noreply.github.com>
2020-04-17 17:51:05 +0300

    Delete SearchByName.js

--- 01 -- Development/js/SearchByName.js
+++
@@ -1,9 +0,0 @@
-const searchByName = () => {
-    let getText = document.getElementById('searchByName').value;
-    let result = searchByNameArr.filter(item => item.name == getText);
-    if (result.length < 1)
-        result = "Данного имени нету в базе данных";
-    console.log(result);
-};
-
-document.getElementById('searchByNameButton').addEventListener('click', searchByName);
