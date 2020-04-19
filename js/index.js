97a5c5858d1ef6351f85ba03e4fe13b543ad7c69
Eliphas-art <52660922+Eliphas-art@users.noreply.github.com>
2020-04-19 15:28:23 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,64 +1,57 @@
-const searchByNameArr = [// json
+const searchByNameArr = [
     {name: "Jonny Walker", birthDate: "1995-12-17"},
     {name: "Andrew", birthDate: "2001-10-29"},
     {name: "Viktor", birthDate: "1998-11-9"},
     {name: "Andrew", birthDate: "2011-05-09"},
+    {name: "Nikitchenko", birthDate: "2011-05-09"},
 ];
+const replaceHtmlDom = (tegName, id, content, idReplace = id) => {
+    const teg = document.createElement(tegName);
+    teg.id = id;
+    teg.innerHTML = content;
+    const afterResult = document.getElementById(idReplace);
+    afterResult.replaceWith(teg);
+};
 
 const getMin = () => {
-    let getText = document.getElementById('getMin').value.split(',');
-
-    let  min = +getText[0];
+    const getText = document.getElementById('getMin').value.split(',');
+    let  min =+ getText[0];
     for (let i = 0; i < getText.length; i++) {
         getText[i] = +getText[i];
         if (getText[i] < min) {
-
             min = getText[i];
-
         }
     }
     document.getElementById('minNameResult').innerHTML = min;
 };
-document.getElementById('getMinButton').addEventListener('click', getMin);
-
 
 const getEven = () => {
     const getText = document.getElementById('getEven').value.split(',');
     const array = [];
-    let len = getText.length;
-    while (len--)
-        if (Math.ceil(getText[len] / 2) == getText[len] / 2) {
-            if (getText[len])
-                array.push(getText[len]);
+    for (let i = 0; i < getText.length; i++)
+        if (Math.ceil(getText[i] / 2) == getText[i] / 2) {
+            if (getText[i])
+                array.push(getText[i]);
         }
     document.getElementById('getEvenResult').innerHTML = array;
 };
-document.getElementById('getEvenButton').addEventListener('click', getEven);
 
 const searchByName = () => {
-    let getText = document.getElementById('searchByName').value;
-    let result = searchByNameArr.filter(item => item.name == getText);
+    const getText = document.getElementById('searchByName').value;
+    const result = searchByNameArr.filter(item => item.name == getText);
     if (result.length < 1) {
-        document.getElementById('searchByNameResult').innerHTML = "Данного пользователя нету в базе данных";
-        let p = document.createElement("p");
-        p.innerHTML = "Данного пользователя нету в базе данных";
-        p.id = "searchByNameResult";
-        let afterResult = document.getElementById("searchByNameResult");
-
-        afterResult.replaceWith(p);
+        replaceHtmlDom("p","searchByNameResult","Данного пользователя нету в базе данных")
     }
     else {
-        let textarea = document.createElement("textarea");
-        let resultOut = [];
-        for (let i =0; i < result.length; i++){
+        const resultOut = [];
+        for (let i=0; i < result.length; i++){
             resultOut.push(result[i].name + ': ' + result[i].birthDate)
         }
-        textarea.innerHTML = resultOut;
-        textarea.id = "searchByNameResult";
-        let afterResult = document.getElementById("searchByNameResult");
-        afterResult.replaceWith(textarea);
+        replaceHtmlDom(resultOut.length > 1 ? "textarea" : "p", "searchByNameResult", resultOut);
     }
     console.log(result);
 };
 
+document.getElementById('getMinButton').addEventListener('click', getMin);
+document.getElementById('getEvenButton').addEventListener('click', getEven);
 document.getElementById('searchByNameButton').addEventListener('click', searchByName);
